421be61c677041c73cc8f54a9c4a4f8b31b414c9
Renu-Madaan <renumadaan.linkedin@gmail.com>
2025-05-21 20:02:43 -0700

    Practice updated

---
+++ TestNG_Framework_Project/src/TestNG_Package/Practicegit.java
@@ -0,0 +1,11 @@
+package TestNG_Package;
+
+import org.testng.annotations.BeforeTest;
+
+public class Practicegit {
+	@BeforeTest
+	public void bus() {
+		System.out.println("This is my old Car1");
+	}
+
+}
